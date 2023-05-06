7104f51be3fa1016ae78dac9a5ed6fd741f93684
Jonny Grobstein <jonnygrobstein@gmail.com>
2023-05-06 17:41:45 -0500

    done app frontend

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components
 
 const Home = () => {
     const router = useRouter();
-
+    const [searchTerm, setSearchTerm] = useState("")
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
             <Stack.Screen 
@@ -32,7 +32,13 @@ const Home = () => {
                     }}
                 >
                     <Welcome
-
+                        searchTerm={searchTerm}
+                        setSearchTerm={setSearchTerm}
+                        handleClick={() => {
+                            if(searchTerm) {
+                                router.push(`/search/${searchTerm}`)
+                            }
+                        }}
                     /> 
 
                     <Popularjobs />
