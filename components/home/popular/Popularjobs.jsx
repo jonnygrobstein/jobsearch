0e0aef45d5199ccdfb2610374eaef420c9638b1a
Jonny Grobstein <jonnygrobstein@gmail.com>
2023-05-06 13:10:12 -0500

    nearby jobs start

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -7,15 +7,21 @@ import { COLORS, SIZES } from '../../../constants'
 import PopularJobCard from '../../common/cards/popular/PopularJobCard'
 import useFetch from '../../../hook/useFetch'
 
+
 const Popularjobs = () => {
   const router = useRouter();
   
   const { data, isLoading, error } = useFetch('search', {
     query: 'React developer',
-    num_pages: 1
+    num_pages: "1",
   })
 
-  console.log(data);
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
   
   return (
     <View style={styles.container}>
@@ -33,10 +39,12 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList 
-            data={[1, 2, 3, 4, 5]}
+            data={[1, 2, 3, 4, 5, 6, 7, 8]}
             renderItem={({ item }) => (
               <PopularJobCard
                 item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
               />
             )}
             keyExtractor={item => item?.job_id}
